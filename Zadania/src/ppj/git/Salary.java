40071a3515fe183d6987ce498c59e4313af1a6bf
Maciej Tymorek <tymorek1@gmail.com>
2018-10-09 14:36:17 +0200

    Mój pierwszy program zapisany w Git

---
+++ Zadania/src/ppj/git/Salary.java
@@ -0,0 +1,10 @@
+package ppj.git;
+
+public class Salary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("pierwszy program java");
+	}
+
+}
